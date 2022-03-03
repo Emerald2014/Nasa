42cef0c147efccf5fed605dbd29fcd8109c508f8
Alex <emerald2014@mail.ru>
2022-03-03 18:49:25 +0300

    Вопрос про цвета TabLayout

--- app/src/main/java/ru/kudesnik/nasa/view/MainActivity.kt
+++ app/src/main/java/ru/kudesnik/nasa/view/MainActivity.kt
@@ -1,14 +1,20 @@
 package ru.kudesnik.nasa.view
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.security.keystore.KeyNotYetValidException
 import ru.kudesnik.nasa.R
 import ru.kudesnik.nasa.view.main.PictureOfTheDayFragment
 
 class MainActivity : AppCompatActivity() {
+    private val KEY_SP = "sp"
+    val KEY_CURRENT_THEME = "current_theme"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(getCurrentTheme())
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
@@ -16,4 +22,27 @@ class MainActivity : AppCompatActivity() {
                 .commitNow()
         }
     }
+
+    fun setCurrentTheme(currentTheme: Int) {
+        val sharedPreferences: SharedPreferences = getSharedPreferences(KEY_SP, MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putInt(KEY_CURRENT_THEME, currentTheme)
+        editor.apply()
+//        recreate()
+
+    }
+
+    private fun getCurrentTheme(): Int {
+        val sharedPreferences: SharedPreferences = getSharedPreferences(KEY_SP, MODE_PRIVATE)
+        return (sharedPreferences.getInt(KEY_CURRENT_THEME, -1))
+    }
+
+//    fun getRealStyle(currentTheme: Int): Int {
+//        when(currentTheme) {
+//            ThemeNasaGreen -> {
+//                return R.style.ThemeNasaGreen
+//            }
+//
+//        }
+//    }
 }

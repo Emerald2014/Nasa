c2f2eb3ba4107890981714a4d040d303098aa3e3
Sukhanov Alex <emerald2014@mail.ru>
2022-03-31 10:17:41 +0300

    Добавил переключение фрагментов и badge

--- app/src/main/java/ru/kudesnik/nasa/view/navigation/BottomNavigationActivity.kt
+++ app/src/main/java/ru/kudesnik/nasa/view/navigation/BottomNavigationActivity.kt
@@ -3,6 +3,7 @@ package ru.kudesnik.nasa.view.navigation
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import ru.kudesnik.nasa.R
 import ru.kudesnik.nasa.databinding.ActivityBottomNavigationBinding
 import ru.kudesnik.nasa.databinding.ActivityNavigationBinding
@@ -17,12 +18,17 @@ class BottomNavigationActivity : AppCompatActivity() {
         binding.bottomNavigationView.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.bottom_view_earth -> {
+                    navigationTo(EarthFragment())
                     true
                 }
                 R.id.bottom_view_mars -> {
+                    navigationTo(MarsFragment())
+
                     true
                 }
                 R.id.bottom_view_system -> {
+                    navigationTo(SystemFragment())
+
                     true
                 }
                 else -> {
@@ -30,5 +36,12 @@ class BottomNavigationActivity : AppCompatActivity() {
                 }
             }
         }
+        binding.bottomNavigationView.selectedItemId = R.id.bottom_view_mars
+        val badge = binding.bottomNavigationView.getOrCreateBadge(R.id.bottom_view_mars)
+        badge.number = 9999
+    }
+
+    private fun navigationTo(f: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.container, f).commit()
     }
 }
